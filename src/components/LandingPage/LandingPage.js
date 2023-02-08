de135998d6fe4bbe75dc48ae25684b1792ed9dc8
ulises Gomez <ulisescap123@gmail.com>
2023-02-08 09:14:49 -0300

    jjjg

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import style from './LandingPage.module.css'
-import dog from './dog.png'
+
 
 
 export class LandingPage extends Component {
@@ -15,7 +15,8 @@ export class LandingPage extends Component {
             <p className={style.p}>then, go to Home and</p>
             <p className={style.p}>look for your favorite dogs </p>
             <div className={style.imgContainer}>
-            <img src={dog} alt='dogs' className={style.img}/>
+            <img src='https://firebasestorage.googleapis.com/v0/b/rentalibre-fbbda.appspot.com/o/dog.png?alt=media&token=5925180b-451a-425e-9af2-9b9543559b65'
+             alt='dogs' className={style.img}/>
             </div>
         </div>
         
